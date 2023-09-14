43b4fce3a3eaafd656e154f97be10bc3db9dc4ee
fabbeCode <fabianlindhe91@gmail.com>
2023-09-14 19:49:50 +0200

    created variables

---
+++ logic.js
@@ -0,0 +1,22 @@
+                
+             function variables() {     var x = 10
+                                        var y = 1/2
+                                        var text = "hey" 
+
+               
+                            if (true) { alert(text + x + y) }
+                
+             
+            
+            }   
+                
+               
+                       
+
+                    
+                
+
+
+                
+                
+    
